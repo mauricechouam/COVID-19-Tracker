a178443533607ca26cdc80abcbd2e77fc6474c0c
mauricechouam <lansichouamou@gmail.com>
2020-05-12 19:45:17 -0700

    Htlm Design suggested

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,10 @@
 // CDC API 2019 Novel Coronavirus
-// https://documenter.getpostman.com/view/8854915/SzS7NkAL?version=latest#9cdd846a-4698-4129-9375-80300fdf8cb1
 
-let queryURL = "https://tools.cdc.gov/api/v2/resources/media/403372.rss"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
 
-});
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 50
+        });
+      }
